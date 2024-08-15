3f43645e8e836d8642c242d16b5a7d1d09b016bd
ryancroix02 <ryancroix02@gmail.com>
2024-08-15 14:24:14 -0400

    created buttons of the pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import {Button} from 'antd'
+import {useState} from 'react'
+import {PoweroffOutlined} from '@ant-design/icons';
+
+function App() 
+{
+  const [loading, setLoading] = useState(false) //the page will load
+  const onButtonClick=(e)=>{  //to call API when button is clicked
+    console.log('Button clicked')
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+
+    }, 2000);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button
+        loading={loading}
+        icon={<PoweroffOutlined />}
+        className='my-button'
+        onClick={onButtonClick}
+        >Homepage</Button>
+        <Button>Investment Page</Button>
+        <Button>Individual Stock Page</Button>
+        <Button>Cash Holdings</Button>
+      
       </header>
     </div>
   );
